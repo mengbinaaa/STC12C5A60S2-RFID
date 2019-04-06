058c5d277a25d8ed0063c7be31a713cc2d1835c1
bmeng <2669630569@qq.com>
2019-04-06 17:21:30 +0800

    add GPL

--- UART0_app.c
+++ UART0_app.c
@@ -1,5 +1,5 @@
 //Git is a distributed version control system
-//Git is a free software.
+//Git is a free software distributed under the GPL.
 /*------------------------------------------------------------------*/
 /* --- STC MCU International Limited -------------------------------*/
 /* --- STC 1T Series MCU Programme Demo ----------------------------*/
